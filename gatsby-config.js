552cbc793a9f5b2dd601cfcaf08a6b5c939c9c53
youngsuncho <youngsunhere@gmail.com>
2021-09-13 15:17:01 +0900

    [MOD] initial personalization2

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,25 +13,7 @@ module.exports = {
       { name: "linkedin", link: "https://www.linkedin.com/in/youngsun-cho-13961a118/" },
     ],
     about: `
-      <p>The son of Jedi Knight Anakin Skywalker and Senator Padmé Amidala,
-      Luke Skywalker was born along with his twin sister Leia in 19 BBY.
-      As a result of Amidala's death and Anakin's fall to the dark side of the Force,
-      the Skywalker children were separated and sent into hiding,
-      with Leia adopted by the royal family of Alderaan while Luke was raised by his relatives on Tatooine.
-      Longing for a life of adventure and purpose,
-      Skywalker joined the Rebellion and began learning the ways of the Force under the guidance of Jedi Master Obi-Wan Kenobi,
-      whose first apprentice was Luke's own father.
-      During the Battle of Yavin in 0 BBY, Skywalker saved the Alliance from annihilation by destroying the Empire's planet-killing superweapon,
-      the Death Star. He continued his training in the years that followed,
-      determined to become a Jedi Knight like his father before him,
-      and found a new mentor in Grand Master Yoda. After his master's death,
-      Skywalker participated in the Battle of Endor in 4 ABY,
-      during which he confronted the Sith Lord Darth Vader,
-      whom he learned was in fact his father, Anakin Skywalker.
-      With Luke's help, Anakin returned to the light side of the Force by destroying the Emperor at the cost of his own life,
-      fulfilling his destiny as the Chosen One. You can find more information <a href="https://starwars.fandom.com/wiki/Luke_Skywalker" target="_blank" rel="noopener noreferrer">here</a>.</p>
-      <p>See the PDF version of my resume <a href="#">here</a>.</p>
-      <p>As you can see, it's possible to add HTML tags in your about page.</p>
+      <p> write <p>See the PDF version of my resume <a href="#">here</a>.</p>
       `,
     education: [
       {
