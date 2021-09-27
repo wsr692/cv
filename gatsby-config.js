2b190409b540d885dbbe0770e62a8c9b052e5bd8
youngsuncho <youngsunhere@gmail.com>
2021-09-27 15:12:16 +0900

    [MOD] bulletting of mediazen cont

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,17 +67,17 @@ module.exports = {
         when: "2019.6 - Present",
         company: "Mediazen",
         where: "Seoul, South Korea",
-        moreInfo: `<p>
-        • Text-to-Speech
-        Implemented Transformer-based TTS (Fastspeech) for Korean and English and
-        experimented with various frame-wise annotation schemes on training data.<br>
+        moreInfo: `
+        
+        <ul>• Text-to-Speech
+        Implemented Transformer-based TTS (Fastspeech) for Korean and English and experimented with various frame-wise annotation schemes on training data.<br>
         – Pre-processed text into phoneme sequences using both custom and open-sourced G2P systems.<br>
         – Pre-processed various types of audio data using signal processing and forced-alignment.<br>
-        
-        • Speech Recognition
+        </ul>
+        <ul>• Speech Recognition
         – Built domain-adapted FST models for AI-assisted contact centers in shopping, foods and hospitality industries. – Built rule-based, light-weight FST models for ASR in subway ticketing kiosks.
-        – Trained DNN-HMM acoustic models that are robust to various indoor and outdoor noises.</p>
-  
+        – Trained DNN-HMM acoustic models that are robust to various indoor and outdoor noises.
+        </ul>
 
         <ul>
         Voice Command System for Subway Ticketing Kiosks, Project Lead Sep. 2020 - Present
