cdcd822b1b7f5fd1770fb1c6727df2e40b752993
youngsuncho <youngsunhere@gmail.com>
2021-09-27 14:08:46 +0900

    [MOD] change to embed url link

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -37,7 +37,7 @@ export default props => {
       pageDescription="Learn more about my experience"
     >
       {experience}
-      <Video videoSrcURL={'https://youtu.be/O2x_DU9-14E?t=34'} videoTitle={'test'}/>
+      <Video videoSrcURL={videoSrcURL={"https://www.youtube.com/embed/O2x_DU9-14E"} videoTitle={'test'}/>
     </Layout>
   )
 }
