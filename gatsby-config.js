c7780970926bb7c4414edc7e4fe2b0ee9aeed544
youngsuncho <youngsunhere@gmail.com>
2021-09-27 23:12:13 +0900

    [MOD] resize video

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,8 +83,8 @@ module.exports = {
         `<ul style="text-indent:-20px; margin-left:20px;">
         Project Lead,<b> <em>Voice Command System for Subway Ticketing Kiosks</em></b>
 
-          <iframe width="420" height="345" src="https://www.youtube.com/embed/O2x_DU9-14E?start=49&end=71">
-</iframe>
+         <center><iframe width="500" height="250" src="https://www.youtube.com/embed/O2x_DU9-14E?start=49&end=71">
+</iframe></center>
 
         <li style="list-style-position:inside; margin-bottom:10px; text-indent: 10px;">Lead development of multi-lingual (ENG, KOR, JAP, CHN) voice-command ticketing system for metropolitan airport express annually servicing over 100 m. passengers.</li>
         <li style="list-style-position:inside; margin-bottom:10px; text-indent: 10px;">Translate client needs into engineering tasks and guided the client over various technical details.</li>
@@ -108,6 +108,13 @@ module.exports = {
         moreInfo: `Trained kaldi-based acoustic model for speech recognition.`,
 
       },
+      {
+        role: "Teaching Assistant",
+        when: "Fall 2016",
+        company: `English Phonology, Korea University`,
+        moreInfo: "Marked assignments, exams, as well as reviewing exam questions.",
+      },
+
       {
         role: "Teaching Assistant",
         when: "2016.1",
