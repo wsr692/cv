90e4af2ef8fde85e6a83664dc6a89b3d034f4eca
youngsuncho <youngsunhere@gmail.com>
2021-09-29 02:03:12 +0900

    [MOD] image src correction

--- src/pages/education.js
+++ src/pages/education.js
@@ -15,7 +15,8 @@ export default props => {
             when
             school
             where
-            moreInfo
+            thesis
+            publications
           }
         }
       }
