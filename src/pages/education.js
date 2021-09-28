637d0c82d4ad81f8d682a5c54bbb2f1bd9a9e7ac
youngsuncho <youngsunhere@gmail.com>
2021-09-29 01:21:43 +0900

    [MOD] image src correction

--- src/pages/education.js
+++ src/pages/education.js
@@ -29,7 +29,7 @@ export default props => {
       when={item.when}
       where={item.where}
       org={item.school}
-      moreInfo={item.moreInfo}
+      moreInfo={item.thesis+img_thesis+item.publications}
     />
   ))
 
@@ -48,4 +48,3 @@ export default props => {
       
     </Layout>
   )
-}
